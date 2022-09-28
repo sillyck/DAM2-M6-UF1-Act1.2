c0d7486ff4f69569eaedbd9128b551b81b5db7fb
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-28 19:24:38 +0200

    Activitat Buida

---
+++ src/copia.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class copia {
+	public static void main (String[] args)
+		throws IOException {
+		
+		// Inicialitzem els fitxers de lectura / escritura
+		
+		
+		
+		try {		//Capturem errors si apareixen.
+
+		//Assignem els noms dels arguments als fitxers
+		//Creem un buffer màxim de lectura
+			
+		// LLegim el fitxer fins al final codi (-1)
+
+		} finally {
+		
+		//Tanquem sempre els fitxers
+
+		}
+	}
+}
