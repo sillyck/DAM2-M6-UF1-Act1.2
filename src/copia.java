4cde81e5f1a3aae59bb49286509ef9d69c5c0d77
Jordi Ribellas <ribellas2002@gmail.com>
2022-09-28 20:44:14 +0200

    Activitat acabada

--- src/copia.java
+++ src/copia.java
@@ -1,24 +1,63 @@
+/*
+ * @author Jordi Ribellas
+ */
+
 import java.io.*;
+import java.util.Scanner;
 
 public class copia {
-	public static void main (String[] args)
-		throws IOException {
+	public static void main (String[] args) throws IOException {
 		
-		// Inicialitzem els fitxers de lectura / escritura
+		Scanner entrada1 = new Scanner(System.in);
+		
+		System.out.println("Escriu la ruta del fitxer d'origen: ");
+				
+		File origen = new File(entrada1.nextLine());
 		
 		
+		Scanner entrada2 = new Scanner(System.in);
 		
-		try {		//Capturem errors si apareixen.
-
-		//Assignem els noms dels arguments als fitxers
-		//Creem un buffer màxim de lectura
+		System.out.println("Escriu la ruta del fitxer d'origen: ");
+				
+		File desti = new File(entrada2.nextLine());
+		
+		
+		if(!origen.exists()) {
+			throw new IOException("El fitxer d'origen no existeix");
+		}
+		
+		if(desti.exists()) {
+			throw new IOException("El fitxer de destí ja existeix");
+		}
+		
+		String ruta = origen.getPath();
+		File rutaArxiu = new File (ruta);
+		
+		
+		if(!rutaArxiu.canWrite()) {
+			throw new IOException("La ruta a la que esta el fitxer no es pot escriure");
+		}
+		
+		FileReader fiRea = new FileReader(origen);
+		FileWriter fiWri = new FileWriter(desti);	
+		
+		try {		
 			
-		// LLegim el fitxer fins al final codi (-1)
-
+			int i=0;
+						
+			char b[] = new char[20];
+			while ((i = fiRea.read(b)) != -1) {
+				fiWri.write(b);
+			}
+			
 		} finally {
 		
-		//Tanquem sempre els fitxers
+			fiRea.close();
+			fiWri.close();
 
 		}
+		
+		System.out.println("Fet!");
 	}
+	
 }
